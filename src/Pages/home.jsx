7a5c024bb21faf0d217277d5589cc8f93a5379db
Dorphia <alex.rybak333@gmail.com>
2024-06-05 18:10:38 -0400

    Started the structure and began work on routes.

---
+++ src/Pages/home.jsx
@@ -0,0 +1,7 @@
+<div>
+<h1>About Me!</h1>
+<p>
+    <h2>Alex Rybak</h2>
+  Lorem ipsum 
+</p>
+</div>
---
+++ src/route.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Home from './pages/home';
+import About from './pages/portfolio';
+import Projects from './pages/resume';
+import Contact from './pages/contact';
+
